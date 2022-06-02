5e9bb4fd8296b0cecb46c2e93bc1c60d35f1f552
Anton Prio Hutomo <antonpriohutomo@gmail.com>
2022-06-02 17:06:58 +0700

    update function SayHello with additional parameter fullname

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/antonprio/go-say-hello
+module github.com/antonprio/go-say-hello/v2
 
 go 1.18
--- say_hello.go
+++ say_hello.go
@@ -1,5 +1,5 @@
 package go_say_hello
 
-func SayHello(name string, lastname string) string {
-	return "Hello " + name + " " + lastname
+func SayHello(name string, lastname string, fullname string) string {
+	return "Hello " + name + " " + lastname + " With Fullname: " + fullname
 }
